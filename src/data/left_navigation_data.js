ef0ecb8a394c430495b930183e46452e764d8415
xiweizhen <xiweizhen@ciicsh.com>
2018-01-13 17:28:19 +0800

    调整目录

--- src/data/left_navigation_data.js
+++ src/data/left_navigation_data.js
@@ -118,8 +118,8 @@ const leftNavigationList = params => Utils.promise({
         children: [
           {cid: "9-1", ckey: "待处理任务", crouter: "pendingTaskList"},
           {cid: "9-2", ckey: "已处理任务", crouter: "processedTaskList"},
-          {cid: "9-3", ckey: "受理单列表", crouter: "acceptanceList"},
-          {cid: "9-4", ckey: "补充医疗理赔", crouter: "supplementaryMedicalList"},
+          {cid: "9-3", ckey: "补充医疗理赔", crouter: "supplementaryMedicalList"},
+          {cid: "9-4", ckey: "受理单列表", crouter: "acceptanceList"},
           {cid: "9-5", ckey: "未投保审核", crouter: "uninsuredReview"},
           {cid: "9-6", ckey: "医疗关系转移", crouter: "relationshipTransfer"},
           {cid: "9-7", ckey: "在保管理", crouter: "insuredList"},

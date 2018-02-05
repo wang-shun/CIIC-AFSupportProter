9b4c4058d95c1a86973477fc344946cc10352791
zhangzhiwen <zhangzhiwen@ciicsh.com>
2018-02-05 13:25:12 +0800

    雇佣退工

--- src/api/employ_manage/hire_operator.js
+++ src/api/employ_manage/hire_operator.js
@@ -2,6 +2,8 @@ import ajax from "../../lib/ajax";
 
 const AJAX = ajax.ajaxGyg;
 
+const saveAmRemark = params => AJAX.postJSON("/api/employcommandservice/amEmpTask/saveAmRemark", params);
+
 export default {
 
   //雇员查询
@@ -27,6 +29,34 @@ export default {
   //保存用工档案
   saveAmArchive:async (params)=>{
     let response = await AJAX.post('/api/employcommandservice/amEmpTask/saveAmArchive', params);
+    return await response.data;
+  },
+  //保存用工档案
+  // saveAmRemark:async (params)=>{
+  //   let response = await AJAX.post('/api/employcommandservice/amEmpTask/saveAmRemark', params);
+  //   return await response.data;
+  // }
+  saveAmRemark,
+  //用工备注删除
+  deleteAmRemark:async (params)=>{
+    let response = await AJAX.post('/api/employcommandservice/amEmpTask/deleteAmRemark', params);
+    return await response.data;
+  }, //退工查询
+  queryAmResign:async (params)=>{
+    let response = await AJAX.post('/api/employcommandservice/amResignTask/queryAmResign', params);
+    return await response.data;
+  },//雇员汇总查询
+  queryResignTaskCount:async (params)=>{
+    let response = await AJAX.post('/api/employcommandservice/amResignTask/queryResignTaskCount', params);
+    return await response.data;
+  },
+  //查询退工详细信息
+  queryAmResignDetail:async (params)=>{
+    let response = await AJAX.post('/api/employcommandservice/amResignTask/queryAmResignDetail', params);
+    return await response.data;
+  },//查询退工详细信息
+  saveAmResign:async (params)=>{
+    let response = await AJAX.post('/api/employcommandservice/amResignTask/saveAmResign', params);
     return await response.data;
   }
 

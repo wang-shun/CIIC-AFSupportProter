bc64ba8f22d4aad34443be8a2fea9335268ac3ed
zhangzhiwen <zhangzhiwen@ciicsh.com>
2018-02-09 18:22:55 +0800

    雇佣工

--- src/components/employ_manage/common/EmployeeInfo.vue
+++ src/components/employ_manage/common/EmployeeInfo.vue
@@ -4,7 +4,7 @@
       <Row type="flex" justify="start">
         <Col :sm="{span: 12}">
           <Form-item label="客户编号：">
-            {{employee.customerNumber}}
+            {{customer.customerNumber}}
           </Form-item>
         </Col>
         <Col :sm="{span: 12}">

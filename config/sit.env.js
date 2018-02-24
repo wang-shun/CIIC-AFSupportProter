b8d0804598b2d9631f0182e9cb0f81a4d9b11231
guwei <guwei@ciicsh.com>
2018-02-24 19:02:20 +0800

    证件管理bug fix

--- config/sit.env.js
+++ config/sit.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   env: '"sit"',
-  SITE_HOST: '"http://172.16.9.24:6025"' // sit环境host
+  SITE_HOST: '"http://172.16.9.24:6025"', // sit环境host
+  SITE_HOST_REPORT_FORM: '"http://172.16.9.24:6017"'
 })
--- src/components/credentials_management/emp_credentials_deal/EmpAdd.vue
+++ src/components/credentials_management/emp_credentials_deal/EmpAdd.vue
@@ -12,21 +12,21 @@
         </Row>
         <Row justify="start" style="margin-top:10px">
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-            <Form-item label="雇员姓名：" prop="name">
-              <Input v-model="formItem.name" placeholder="请输入"/>
+            <Form-item label="雇员姓名：" prop="employeeName">
+              <Input v-model="formItem.employeeName" placeholder="请输入"/>
             </Form-item>    
           </i-col>
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-            <Form-item label="性别：" prop="sex">
-              <Select v-model="formItem.sex" placeholder="请选择" transfer>
+            <Form-item label="性别：" prop="gender">
+              <Select v-model="formItem.gender" placeholder="请选择" transfer>
                 <Option value="1">男</Option>
                 <Option value="0">女</Option>
               </Select>
             </Form-item>
           </i-col>
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-            <Form-item label="国籍：" prop="country">
-              <Select v-model="formItem.country" placeholder="请选择" transfer>
+            <Form-item label="国籍：" prop="countryCode">
+              <Select v-model="formItem.countryCode" placeholder="请选择" transfer>
                 <Option v-for="(value,key) in this.baseDic.country" :value="key" :key="key">{{ value }}</Option>
               </Select>
             </Form-item>    
@@ -39,8 +39,8 @@
             </Form-item>    
           </i-col>
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}" style="margin-top:10px">
-            <Form-item label="证件号码：" prop="IDCardNum">
-              <Input v-model="formItem.IDCardNum" placeholder="请输入"/>
+            <Form-item label="证件号码：" prop="idNum">
+              <Input v-model="formItem.idNum" placeholder="请输入"/>
             </Form-item>    
           </i-col>
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}" style="margin-top:10px">
@@ -49,13 +49,13 @@
             </Form-item> 
           </i-col>
         </Row>
-        <Row justify="start" style="margin-top:10px">
+        <!-- <Row justify="start" style="margin-top:10px">
           <i-col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
             <Form-item label="备注：" prop="remark">
               <Input v-model="formItem.remark" type="textarea" :autosize="{minRows: 3,maxRows: 6}" placeholder="请输入"/>
             </Form-item>
           </i-col>
-        </Row>
+        </Row> -->
         <Row justify="start" class="tr">
           <i-col :sm="{span: 24}">
             <Button type="primary" @click="submit('empAddForm')" class="ml10" >保存</Button>
@@ -69,37 +69,39 @@
 
 <script>
 import InputCompanyName from '../../common_control/form/input_company/InputCompanyName.vue'
+import axios from 'axios'
 
+const host = process.env.SITE_HOST
 export default {
   components:{InputCompanyName},
   data () {
     return {
       formItem: {
         company: '',
-        name: '',
-        sex: '',
-        country: '',
+        employeeName: '',
+        gender: '',
+        countryCode: '',
         IDCardType: '',
-        IDCardNum: '',
+        idNum: '',
         birthday: '',
         remark: ''
       },
       empAddForm: {
         company: '',
-        name: '',
-        sex: '',
-        country: '',
+        employeeName: '',
+        gender: '',
+        countryCode: '',
         IDCardType: '',
-        IDCardNum: '',
+        idNum: '',
         birthday: ''
       },
       ruleValidate: {
         company: [{ required: true, message: '请选择客户', trigger: 'change' }],
-        name: [{ required: true, message: '雇员姓名不能为空', trigger: 'blur' }],
-        sex: [{ required: true, message: '请选择性别', trigger: 'change' }],
-        country: [{ required: true, message: '请选择国籍', trigger: 'change' }],
+        employeeName: [{ required: true, message: '雇员姓名不能为空', trigger: 'blur' }],
+        gender: [{ required: true, message: '请选择性别', trigger: 'change' }],
+        countryCode: [{ required: true, message: '请选择国籍', trigger: 'change' }],
         IDCardType: [{ required: true, message: '请选择证件类型', trigger: 'change' }],
-        IDCardNum: [{ required: true, message: '证件号码不能为空', trigger: 'blur' }],
+        idNum: [{ required: true, message: '证件号码不能为空', trigger: 'blur' }],
         birthday: [{ required: true, type: 'date', message: '请选择出生日期', trigger: 'change' }]
       }
     }
@@ -108,9 +110,27 @@ export default {
     submit (value) {
       this.$refs[value].validate((valid) => {
         if (valid) {
-          this.$Message.success('保存成功!')
+          axios.post(host + '/api/emp/add',this.formItem).then((response) => {
+            if (response.data.errCode === '0'){
+               this.$Notice.success({
+                  title: '保存成功',
+                  desc: ''
+                })
+                this.find()
+            } else {
+              this.$Notice.error({
+                title: '保存失败',
+                desc: ''
+              })
+            }
+          }).catch((error) => {
+            this.$Notice.error({
+              title: '保存失败',
+              desc: ''
+            })
+          })
         } else {
-          this.$Message.error('保存失败!')
+          this.$Message.error('校验失败!')
         }
       })
     },

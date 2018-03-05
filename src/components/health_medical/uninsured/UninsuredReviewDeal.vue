7884f50701ecd15ce0320e23c3afd9d5f6a11305
xiweizhen <xiweizhen@sina.cn>
2018-03-05 19:05:48 +0800

    受理单

--- src/components/health_medical/uninsured/UninsuredReviewDeal.vue
+++ src/components/health_medical/uninsured/UninsuredReviewDeal.vue
@@ -84,6 +84,7 @@
 
 <script>
   import admissibility from '../../../store/modules/health_medical/data_sources/admissibility.js'
+  import apiAjax from "../../../data/health_medical/uninsured_application.js";
 
   export default {
     data() {
@@ -111,9 +112,20 @@
     },
     methods: {
       addUninsuredReviewDeal() {
+        console.info("======");
         this.$refs['formItem'].validate((valid) => {
           if (valid) {
-            this.$router.push({name: "uninsuredReview"})
+            console.info("======");
+            apiAjax.addUninsuredAudit(this.formItem).then(response => {
+              if (response.data.code === 200) {
+                this.$router.push({name: "uninsuredReview"})
+              } else {
+                this.$Message.error("服务器异常，请稍后再试");
+              }
+            }).catch(e => {
+              console.info(e.message);
+              this.$Message.error("服务器异常，请稍后再试");
+            });
           }
         });
       },

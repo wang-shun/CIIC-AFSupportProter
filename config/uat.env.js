84e0c67383d394bb1874f2db19e738ebd5a9a726
guwei <guwei@ciicsh.com>
2018-03-09 19:44:32 +0800

    uat host配置

--- config/uat.env.js
+++ config/uat.env.js
@@ -3,4 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   env: '"uat"',
+  SITE_HOST: '"http://172.16.9.61:6025"', // uat环境host
+  SITE_HOST_REPORT_FORM: '"http://172.16.9.61:6017"'
 })

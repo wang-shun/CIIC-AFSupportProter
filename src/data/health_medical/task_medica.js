e60773ac848b581d9f61742b3b17fcfd6f80d38b
xiweizhen <xiweizhen@ciicsh.com>
2018-03-22 10:22:34 +0800

    功能优化

--- src/data/health_medical/task_medica.js
+++ src/data/health_medical/task_medica.js
@@ -7,8 +7,10 @@ const ajaxApi = ajax.ajaxHmc;
 
 const queryTaskPage = params => ajaxApi.postJSON("/api/afsupportcenter/healthmedical/afTpaTask/queryTaskPage", params);
 const updateTpaTask = params => ajaxApi.postJSON("/api/afsupportcenter/healthmedical/afTpaTask/updateTpaTask", params);
+const syncToWarranty = params => ajaxApi.postJSON("/api/afsupportcenter/healthmedical/afTpaTask/syncToWarranty", params);
 
 export default {
   queryTaskPage,
   updateTpaTask,
+  syncToWarranty,
 }

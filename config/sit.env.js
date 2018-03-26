5657e657dbdd08cb9d5bf11946f5b6c6df03b5b6
guwei <guwei@ciicsh.com>
2018-03-26 09:27:40 +0800

    host update

--- config/sit.env.js
+++ config/sit.env.js
@@ -2,8 +2,8 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  env: '"sit"',
-  SITE_HOST: '"http://172.16.9.29:6025"', // sit环境host
-  SITE_HOST_REPORT_FORM: '"http://172.16.9.29:6017"',
-  HOST_SUPPLEMENTMEDICAL: '"http://172.16.9.29:6015"' //健康医疗
+    env: '"sit"',
+    SITE_HOST: '"http://172.16.9.29:6025"', // sit环境host
+    SITE_HOST_REPORT_FORM: '"http://172.16.9.29:6017"',
+    HOST_SUPPLEMENTMEDICAL: '"http://172.16.9.29:6015"' //健康医疗
 })

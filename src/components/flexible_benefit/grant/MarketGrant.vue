976ae7b4ea0e01d8fb650d5871510e0acbe912e2
yujiawei <yujiawei@ciicsh.com>
2018-04-03 17:37:00 +0800

    修复市场活动发放的前端bug

--- src/components/flexible_benefit/grant/MarketGrant.vue
+++ src/components/flexible_benefit/grant/MarketGrant.vue
@@ -7,22 +7,23 @@
           <Form :label-width="150">
             <Row class="m10">
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="申请单号: ">{{ applyRecord.applyRecordId }}</Form-item>
+              <Form-item label="申请单号: ">{{ applyRecord.applyRecordId }}</Form-item>
               </Col>
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="主题: ">{{ applyRecord.projectTopics }}</Form-item>
+              <Form-item label="主题: ">{{ applyRecord.projectTopics }}</Form-item>
               </Col>
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="申请类别: ">活动申请</Form-item>
+              <Form-item label="申请类别: ">活动申请</Form-item>
               </Col>
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="申请人部门: ">{{ applyRecord.contactDeptName }}</Form-item>
+              <Form-item label="申请人部门: ">{{ applyRecord.contactDeptName }}</Form-item>
               </Col>
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="申请人职位: ">{{ applyRecord.contactPosition }}</Form-item>
+              <Form-item label="申请人职位: ">{{ applyRecord.contactPosition }}</Form-item>
               </Col>
               <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 8}">
-                <Form-item label="申请时间: ">{{this.$utils.formatDate(applyRecord.applyTime, 'YYYY-MM-DD HH:mm:ss')}}</Form-item>
+              <Form-item label="申请时间: ">{{this.$utils.formatDate(applyRecord.applyTime, 'YYYY-MM-DD HH:mm:ss')}}
+              </Form-item>
               </Col>
             </Row>
           </Form>
@@ -31,7 +32,8 @@
       <Panel name="2">
         申请明细
         <div slot="content">
-          <Table stripe border :columns="applyDetailColumns" :data="recordDetailList" ref="applyDetailTable"></Table>
+          <Table stripe border :columns="applyDetailColumns" :data="recordDetailList"
+                 @on-selection-change="getSelection" ref="applyDetailTable"></Table>
         </div>
       </Panel>
       <Panel name="3">
@@ -39,18 +41,19 @@
         <div slot="content">
           <Row>
             <Col :xs="{span: 8, offset: 1}" :lg="{ span: 8, offset: 1}">
-              <Form :label-width=120>
-                <Form-item label="发放备注：">
-                  <Input v-model="sendRemark" type="textarea" :autosize="{minRows: 3,maxRows: 5}" placeholder=""/>
-                </Form-item>
-              </Form>
+            <Form :label-width=120>
+              <Form-item label="发放备注：">
+                <Input v-model="sendRemark" type="textarea" :autosize="{minRows: 3,maxRows: 5}" placeholder=""/>
+              </Form-item>
+            </Form>
             </Col>
           </Row>
           <Row type="flex" justify="start">
             <Col :sm="{span: 24}" class="tr">
-              <Button type="warning" @click="back()">返回</Button>
-              <Button type="primary" @click="grantMarket(2)">发放</Button>
-              <Button type="error" @click="grantMarket(3)">批退</Button>
+            <Button type="primary" @click="grantMarket(2)">发放</Button>
+            <Button type="error" @click="grantMarket(3)">批退</Button>
+            <Button type="warning" @click="back()">返回</Button>
+
             </Col>
           </Row>
         </div>
@@ -82,7 +85,7 @@
             render: (h, params) => {
               return h(marketTableExpand, {
                 props: {
-                  examineData: params.row.examineData
+                  examineData: params.row.approvalStepList
                 }
               })
             }
@@ -99,18 +102,18 @@
           {
             title: '职位', key: 'contactPosition', align: 'center',
           },
-          {
-            //客服中心数据
-            title: '客户服务', key: 'date7', align: 'center',
-          },
+          // {
+          //   //客服中心数据
+          //   title: '客户服务', key: 'date7', align: 'center',
+          // },
           {
             title: '数量', key: 'number', align: 'center',
           },
           {
             title: '审批后数量', key: 'approvalNumber', align: 'center',
             render: (h, params) => {
-              return h("Input",{
-                props:{value: params.row.approvalNumber},
+              return h("Input", {
+                props: {value: params.row.approvalNumber},
                 on: {
                   'on-change': (e) => {
                     this.$set(this.recordDetailList[params.index], 'approvalNumber', e.target.value)
@@ -123,17 +126,33 @@
             title: '派送地址', key: 'deliveryAddress', align: 'center',
           },
           {
-            title: '状态', key: 'approvalStatus', align: 'center',
+            title: '审批状态', key: 'approvalStatus', align: 'center',
             render: (h, params) => {
               switch (params.row.approvalStatus) {
-                case 0:
+                case 1:
                   return "审批中";
                   break;
-                case 1:
+                case 2:
                   return "同意";
                   break;
-                case 2:
+                case 3:
                   return "不同意";
+                  break;
+              }
+            }
+          },
+          {
+            title: '发放状态', key: 'sendStatus', align: 'center',
+            render: (h, params) => {
+              switch (params.row.sendStatus) {
+                case 1:
+                  return "未处理";
+                  break;
+                case 2:
+                  return "已发放";
+                  break;
+                case 3:
+                  return "已批退";
                   break;
               }
             }
@@ -151,11 +170,16 @@
         this.selection = selection;
       },
       selectMarketGrantInformation(val) {
+        const CURRENT_USER = JSON.parse(window.sessionStorage.getItem('userInfo'));
         apiAjax.queryMarketInformation(val).then(response => {
           this.applyRecord = response.data.object.applyRecord;
           this.recordDetailList = response.data.object.recordDetailList;
+          for (let i = 0, len = this.recordDetailList.length; i < len; i++) {
+            this.recordDetailList[i]['_disabled'] = this.recordDetailList[i].approvalStatus !== 2;
+
+          }//for
         }).catch(e => {
-          console.info(e.message);
+          // console.info(e.message);
           this.$Message.error("服务器异常，请稍后再试");
         });
       },
@@ -180,7 +204,7 @@
             this.$Message.error("服务器异常，请稍后再试");
           }
         }).catch(e => {
-          console.info(e.message);
+          // console.info(e.message);
           this.$Message.error("服务器异常，请稍后再试");
         });
       },

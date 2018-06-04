1b3318f6594ef2a78f5609de91159aeece5559de
xiweizhen <xiweizhen@ciicsh.com>
2018-06-04 15:06:02 +0800

    导入校验优化

--- src/components/health_medical/medical_supplementary/SupplementaryMedicalList.vue
+++ src/components/health_medical/medical_supplementary/SupplementaryMedicalList.vue
@@ -315,7 +315,7 @@
           if (response.data.code === 200) {
             this.$Message.success("上传成功");
           }else if (response.data.code === 400){
-            this.$Message.success(response.data.message);
+            this.$Message.error(response.data.message);
           } else {
             this.$Message.error("服务器异常，请稍后再试");
           }

8785f63d1cb3be518c3e4f4c88ff58ec06228b08
likai <likai@ciicsh.com>
2018-07-05 19:53:19 +0800

    LPT环境配置

---
+++ config/lpt.env.js
@@ -0,0 +1,14 @@
+var merge = require('webpack-merge')
+var prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  env: '"lpt"',
+  COMMON_SERVICE_HOST: '"http://172.16.9.68:9621"',
+  SYSTEM_MANAGE_SERVICE_HOST: '"http://172.16.9.68:2003"',
+  TOKEN_URL: '"http://172.16.9.68:8070/#/menu"',
+  LOGIN_URL: '"http://172.16.9.68:8070"',
+  LOCAL_URL: '"http://172.16.9.68"',
+  SITE_HOST: '"http://172.16.9.69:6025"', // lpt环境host
+  SITE_HOST_REPORT_FORM: '"http://172.16.9.69:6017"',
+  SERVER_HOST: '"http://172.16.9.68"'
+})

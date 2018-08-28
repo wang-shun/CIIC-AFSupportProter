e649c581834ed35d426b25efe8dc8b57f928bbf3
xiweizhen <xiweizhen@ciicsh.com>
2018-08-28 10:53:42 +0800

    批退原因需显示

--- src/components/health_medical/medical_task_management/ProcessedTaskList.vue
+++ src/components/health_medical/medical_task_management/ProcessedTaskList.vue
@@ -193,7 +193,16 @@
             align: "center"
           },
           {
-            title: "投保类型", sortable: true, key: "productName", align: "center", width: 150
+            title: "投保类型", sortable: true, key: "taskType", align: "center", width: 120,
+            render: (h, params) => {
+              return h('div', task.taskTypeToChinese(params.row.taskType));
+            }
+          },
+          {
+            title: "产品名称", sortable: true, key: "productName", align: "center", width: 150
+          },
+          {
+            title: "操作说明", sortable: true, key: "remark", align: "center", width: 150
           },
           {
             title: "雇员编号", sortable: true, key: "employeeId", align: "center", width: 150

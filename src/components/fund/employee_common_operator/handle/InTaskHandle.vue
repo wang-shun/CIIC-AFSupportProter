0cca58d65f8323a2fb333d522d3d2119e00bad25
linhui <linhui@ciicsh.com>
2018-09-04 16:43:03 +0800

    v1.2.1

--- src/components/fund/employee_common_operator/handle/InTaskHandle.vue
+++ src/components/fund/employee_common_operator/handle/InTaskHandle.vue
@@ -1410,7 +1410,7 @@
             if (!data.data || data.data.length == 0) {
               this.isShowPrint = true;
               //赋值 转入和转出的默认值
-            // this.transferOutUnitList.push('市公积金封存办(中心)');
+              this.transferOutUnitList.push('市公积金封存办(中心)');
               console.log(this.transferNotice.transferOutUnit);
               this.transferNotice.transferOutUnit = '市公积金封存办(中心)';
               console.log(this.transferNotice.transferOutUnit);

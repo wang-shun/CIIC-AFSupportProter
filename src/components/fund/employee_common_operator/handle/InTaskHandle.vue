3dbf353704a3134e04d1b8c02abb23da82ec2b32
linhui <linhui@ciicsh.com>
2018-09-05 17:20:25 +0800

    fix

--- src/components/fund/employee_common_operator/handle/InTaskHandle.vue
+++ src/components/fund/employee_common_operator/handle/InTaskHandle.vue
@@ -1413,12 +1413,17 @@
               //this.transferOutUnitList.push('市公积金封存办(中心)');
               console.log(this.transferNotice.transferOutUnit);
               let self=this;
-              //setTimeout(function(){self.transferNotice.transferOutUnit = '市公积金封存办(中心)';},1000);
-              this.transferNotice.transferOutUnit = '市公积金封存办(中心)';
+              setTimeout(()=>{
+                self.transferNotice.transferOutUnit = '市公积金封存办(中心)';
+                this.isShowPrint = true;
+              },500);
+              //this.transferNotice.transferOutUnit = '市公积金封存办(中心)';
 
               console.log(this.transferNotice.transferOutUnit);
               this.transferNotice.transferOutUnitAccount = '881383288';
-              this.transferInUnitList.push(this.displayVO.comAccountName);
+              if(this.displayVO.comAccountName!=''){
+                this.transferInUnitList.push(this.displayVO.comAccountName);
+              }
               this.transferNotice.transferInUnit = this.displayVO.comAccountName;
               if(this.displayVO.hfType==1){
                 this.transferNotice.transferInUnitAccount = this.displayVO.basicHfComAccount;
@@ -1426,7 +1431,7 @@
                 this.transferNotice.transferInUnitAccount = this.displayVO.addedHfComAccount;
               }
               this.transferNotice.transferDate=new Date();
-              this.isShowPrint = true;
+              
             } else {
               //transapi.printTransferTask({empTaskId: data.data.empTaskId})
               let params={empTaskId: data.data.empTaskId};

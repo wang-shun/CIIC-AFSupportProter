7c44dee8455329624f68b97de9632a70812acb2b
linhui <linhui@ciicsh.com>
2018-09-05 11:33:34 +0800

    fix bug

--- src/components/fund/employee_common_operator/handle/InTaskHandle.vue
+++ src/components/fund/employee_common_operator/handle/InTaskHandle.vue
@@ -1412,7 +1412,7 @@
               //赋值 转入和转出的默认值
               this.transferOutUnitList.push('市公积金封存办(中心)');
               console.log(this.transferNotice.transferOutUnit);
-              setTimeout(this.transferNotice.transferOutUnit = '市公积金封存办(中心)',500);
+              setTimeout(function(){this.transferNotice.transferOutUnit = '市公积金封存办(中心)';},1000);
               console.log(this.transferNotice.transferOutUnit);
               this.transferNotice.transferOutUnitAccount = '881383288';
               this.transferInUnitList.push(this.displayVO.comAccountName);
--- src/components/fund/employee_transfer_operator/EmployeeFundTransferProgressTwo.vue
+++ src/components/fund/employee_transfer_operator/EmployeeFundTransferProgressTwo.vue
@@ -369,9 +369,9 @@
           this.transferNotice.transferOutUnit = this.displayVO.comAccountName;
           this.transferOutUnitList.push(this.displayVO.comAccountName);
           if(this.$route.query.hfType ==undefined ||this.$route.query.hfType== '1'){ //基本公积金
-            setTimeout(this.transferNotice.transferOutUnitAccount=this.displayVO.basicHfComAccount,500);
+            setTimeout(function(){this.transferNotice.transferOutUnitAccount=this.displayVO.basicHfComAccount;},500);
           }else{
-            setTimeout(this.transferNotice.transferOutUnitAccount=this.displayVO.addedHfComAccount,500);
+            setTimeout(function(){this.transferNotice.transferOutUnitAccount=this.displayVO.addedHfComAccount;},500);
           }
           
 //            setTimeout(this.setValue,500);

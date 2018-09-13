492f1d0211e43abeef7856332938c5e65ea4ad0a
zhangzhiwen <zhangzhiwen@ciicsh.com>
2018-09-13 18:55:54 +0800

    样式

--- src/components/employ_manage/common/EmployeeCompleteInfo.vue
+++ src/components/employ_manage/common/EmployeeCompleteInfo.vue
@@ -159,7 +159,9 @@
         </Col>
         <Col :sm="{span: 22}" :md="{span: 12}" :lg="{span: 16}">
           <Form-item label="公司特殊情况：">
-            {{employee.employSpecial}}
+            <RadioGroup v-model="employee.speacilStr" type="button" >
+               <Radio v-for="item in employee.speacilStr" :label="item" :key="item" :disabled=true></Radio>
+            </RadioGroup>
           </Form-item>
         </Col>
       </Row>

2d039b75fbab059e31f2c35b131bbe06cf254d67
xiweizhen <xiweizhen@ciicsh.com>
2018-09-18 16:30:53 +0800

    任务单添加切换公司功能

--- src/components/health_medical/medical_task_management/PendingTaskList.vue
+++ src/components/health_medical/medical_task_management/PendingTaskList.vue
@@ -131,6 +131,9 @@
     </Collapse>
 
     <div class="tr m20">
+      <Button type="info" v-if="formItem.afProductId===19 || formItem.afProductId===11" @click="modalCom = true">
+        切换公司
+      </Button>
       <Button type="info" v-if="formItem.status===null || formItem.status==='' || formItem.status==='2'"
               @click="modal1 = true">审核
       </Button>
@@ -154,6 +157,23 @@
               icon="ios-download-outline">导出数据
       </Button>
     </div>
+
+    <Modal class="warn-back"
+           v-model="modalCom"
+           title="切换公司">
+      <label>
+        <Select v-model="companyId">
+          <Option v-for="item in insuranceCompanyProperties" :value="item.insuranceCompanyId"
+                  :key="item.insuranceCompanyId">
+            {{item.insuranceCompanyName}}
+          </Option>
+        </Select>
+      </label>
+      <div slot="footer">
+        <Button @click="cancelCom()">取消</Button>
+        <Button type="primary" @click="switchCompany" :loading="loading">提交</Button>
+      </div>
+    </Modal>
 
     <Modal class="warn-back"
            v-model="modal1"
@@ -235,6 +255,7 @@
   export default {
     data() {
       return {
+        modalCom: false,
         modal1: false,
         modal2: false,
         modal3: false,
@@ -264,6 +285,7 @@
           insuranceCompany: null
         },
         syncDate: null,
+        companyId: '1',
         dealMsg: {
           remark: ''
         },
@@ -473,6 +495,38 @@
           });
         }
       },
+      switchCompany() {
+        if (this.formItem.afProductId === 11 && this.companyId === '1') {
+          this.$Message.info("不能切换到相同的保险公司");
+          return;
+        }
+        if (this.formItem.afProductId === 19 && this.companyId === '2') {
+          this.$Message.info("不能切换到相同的保险公司");
+          return;
+        }
+        if (this.selectData.length === 0) {
+          this.$Message.error("请选择数据");
+          return;
+        }
+        let date = {};
+        date.insurancePolicyID = this.formItem.afProductId;
+        date.insuranceCompanyID = this.companyId;
+        date.afTpaTasks = this.selectData;
+        apiAjax.changeInsuranceCompany(date).then(response => {
+          if (response.data.code === 200) {
+            this.getByPage(1);
+            this.cancelCom();
+            this.selectData = [];
+            this.$Message.success("更新成功");
+          } else {
+            this.$Message.error("服务器异常，请稍后再试");
+          }
+        }).catch(e => {
+          console.info(e.message);
+          this.$Message.error("服务器异常，请稍后再试");
+        });
+
+      },
       cancel(val) {
         if (val === 4) {
           this.modal1 = false;
@@ -484,6 +538,9 @@
           this.modal2 = false;
         }
         this.dealMsg.remark = ''
+      },
+      cancelCom() {
+        this.modalCom = false;
       },
       syncToWarranty() {
         if (!this.syncDate) {
@@ -536,6 +593,10 @@
         });
       },
       queryIcProductRelationInfo(val) {
+        //如果传入没值则返回
+        if (!val) {
+          return;
+        }
         apiAjax.queryIcProductRelation(val).then(response => {
           if (response.data.code === 200) {
             this.taskTypeItem = response.data.object;

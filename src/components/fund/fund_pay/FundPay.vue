ffe9e26024eb050b7cdeaa821066e742b752069b
linhui <linhui@ciicsh.com>
2018-09-26 13:24:09 +0800

    1.2.2

--- src/components/fund/fund_pay/FundPay.vue
+++ src/components/fund/fund_pay/FundPay.vue
@@ -1004,7 +1004,8 @@
          row.paymentBank = this.operateEditData[0].paymentBank;
          row.fundAccountType = this.operateEditData[0].fundAccountType;
          row.paymentMonthValue = this.operateEditData[0].paymentMonth;
-         row.paymentStatus = this.operateEditData[0].paymentStatus;
+         //row.paymentStatus = this.operateEditData[0].paymentStatus;
+         row.paymentStatus = 1;
          let params = {
            pageSize: 99999,//暂时这么改，后续把分页去掉
            pageNum: 1,

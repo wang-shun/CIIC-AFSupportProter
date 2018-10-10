bd76ffc26f848ca36291f6a8cef5cd7c4de8db78
likai <likai@ciicsh.com>
2018-10-10 17:34:31 +0800

    batch TBC

--- src/components/fund/employee_common_operator/tab/NoProcess.vue
+++ src/components/fund/employee_common_operator/tab/NoProcess.vue
@@ -11,8 +11,6 @@
 
     <Row class="mt20">
       <Col :sm="{span: 24}" class="tr">
-        <Button type="info" @click="openBatchHandle()">批量办理</Button>
-        <Button type="info" @click="openBatchList()">批量办理批次</Button>
         <Button type="error" @click="openReject()">批量批退</Button>
         <Button type="info"  @click="isUpload=true">批量预录入雇员公积金账号</Button>
         <Button type="info" @click="excelExport()">导出</Button>
@@ -105,22 +103,6 @@
         <Button type="warning" @click="isShowRejectBatch = false">取消</Button>
       </div>
     </Modal>
-
-    <Modal
-      :width="1000"
-      v-model="isShowBatchHandle"
-      @on-ok="batchOk"
-      @on-cancel="batchCancel">
-      <inBatchHandle></inBatchHandle>
-    </Modal>
-
-    <Modal
-      :width="1200"
-      v-model="isShowBatchList"
-      @on-ok="batchListOk"
-      @on-cancel="batchListCancel">
-      <batchList></batchList>
-    </Modal>
   </div>
 </template>
 <script>
@@ -131,11 +113,9 @@
   import sessionData from '../../../../api/session-data'
   import searchEmployee from "./SearchEmployee.vue"
   import tableStyle from '../../../../api/table_style'
-  import inBatchHandle from "../batch_handle/InTaskBatchHandle.vue"
-  import batchList from "../batch_handle/BatchList.vue"
 
   export default {
-    components: {InputCompany,searchEmployee,inBatchHandle,batchList},
+    components: {InputCompany,searchEmployee},
     data() {
       return {
         collapseInfo: [1], //展开栏

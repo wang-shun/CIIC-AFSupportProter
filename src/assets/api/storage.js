329df85213919c9978cf52c0cde668fc6b216e15
linhui <linhui@ciicsh.com>
2018-10-12 23:52:26 +0800

    storage.js 升级

--- src/assets/api/storage.js
+++ src/assets/api/storage.js
@@ -3,59 +3,75 @@
  */
 let localStorage = {
   getItem: function(key) {
-    var value = window.localStorage.getItem(`test_${key}`);
+    var value = key !== 'userInfo' ? window.localStorage.getItem(`test_${key}`) : window.localStorage.getItem(key)
     if (value) {
       try {
-        var value_json = JSON.parse(value);
+        var value_json = JSON.parse(value)
         if (typeof value_json === 'object') {
-          return value_json;
+          return value_json
         } else if (typeof value_json === 'number') {
-          return value_json;
+          return value_json
         }
       } catch(e) {
-        return value;
+        return value
       }
     } else {
-      return false;
+      return null
     }
   },
   setItem: function(key, value) {
-    window.localStorage.setItem(`test_${key}`, value);
+    if (key !== 'userInfo') {
+      window.localStorage.setItem(`test_${key}`, value)
+    } else {
+      window.localStorage.setItem(key, value)
+    }
   },
   removeItem: function(key) {
-    window.localStorage.removeItem(`test_${key}`);
+    if (key !== 'userInfo') {
+      window.localStorage.removeItem(`test_${key}`)
+    } else {
+      window.localStorage.removeItem(key)
+    }
   },
   clear: function() {
-    window.localStorage.clear();
+    window.localStorage.clear()
   }
 }
 
 let sessionStorage = {
   getItem: function(key) {
-    var value = window.sessionStorage.getItem(`test_${key}`);
+    var value = key !== 'userInfo' ? window.sessionStorage.getItem(`test_${key}`) : window.sessionStorage.getItem(key)
     if (value) {
       try {
-        var value_json = JSON.parse(value);
+        var value_json = JSON.parse(value)
         if (typeof value_json === 'object') {
-          return value_json;
+          return value_json
         } else if (typeof value_json === 'number') {
-          return value_json;
+          return value_json
         }
       } catch(e) {
-        return value;
+        return value
       }
     } else {
-      return false;
+      return null
     }
   },
   setItem: function(key, value) {
-    window.sessionStorage.setItem(`test_${key}`, value);
+    if (key !== 'userInfo') {
+      window.sessionStorage.setItem(`test_${key}`, value)
+    } else {
+      window.sessionStorage.setItem(key, value)
+    }
   },
   removeItem: function(key) {
-    window.sessionStorage.removeItem(`test_${key}`);
+    if (key !== 'userInfo') {
+      window.sessionStorage.removeItem(`test_${key}`)
+    } else {
+      window.localStorage.removeItem(key)
+    }
   },
   clear: function() {
-    window.sessionStorage.clear();
+    window.sessionStorage.clear()
   }
 }
 

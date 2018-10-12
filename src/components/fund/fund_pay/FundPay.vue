2bce04ebe88ae6d3af6e394058bd869f2a23c668
linhui <linhui@ciicsh.com>
2018-10-12 14:50:32 +0800

    1.3

--- src/components/fund/fund_pay/FundPay.vue
+++ src/components/fund/fund_pay/FundPay.vue
@@ -972,9 +972,9 @@
         let row;
         row=this.checkSelect();
         if(!row)return false;
-        // 支付状态: 1 ,未到账(默认)   2,送审   3 汇缴(已申请到财务部 ) 4  财务部批退  5,财务部审批通过  6 出票 7  回单
+        // 支付状态: 0 无需支付 1 ,未到账(默认)   2,送审   3 汇缴(已申请到财务部 ) 4  财务部批退  5,财务部审批通过  6 出票 7  回单
         // 未到账和送审才允许编辑
-        if(row.paymentState != 1 && row.paymentState != 2 && row.paymentState != 4){
+        if(row.paymentState != 0 && row.paymentState != 1 && row.paymentState != 2 && row.paymentState != 4){
           this.$Message.info("当前状态，不允许编辑！");
           return false;
         }

8113eba13d63b70d4463ab0a08d046af26d7ddd6
likai <likai@ciicsh.com>
2018-10-13 01:10:32 +0800

    localStorage,sessionStorage

--- src/assets/api/storage.js
+++ src/assets/api/storage.js
@@ -3,32 +3,19 @@
  */
 let localStorage = {
   getItem: function(key) {
-    var value = key !== 'userInfo' ? window.localStorage.getItem(`test_${key}`) : window.localStorage.getItem(key)
-    if (value) {
-      try {
-        var value_json = JSON.parse(value)
-        if (typeof value_json === 'object') {
-          return value_json
-        } else if (typeof value_json === 'number') {
-          return value_json
-        }
-      } catch(e) {
-        return value
-      }
-    } else {
-      return null
-    }
+    var value = key !== 'userInfo' ? window.localStorage.getItem(`afSupportCenter_${key}`) : window.localStorage.getItem(key)
+    return value
   },
   setItem: function(key, value) {
     if (key !== 'userInfo') {
-      window.localStorage.setItem(`test_${key}`, value)
+      window.localStorage.setItem(`afSupportCenter_${key}`, value)
     } else {
       window.localStorage.setItem(key, value)
     }
   },
   removeItem: function(key) {
     if (key !== 'userInfo') {
-      window.localStorage.removeItem(`test_${key}`)
+      window.localStorage.removeItem(`afSupportCenter_${key}`)
     } else {
       window.localStorage.removeItem(key)
     }
@@ -40,7 +27,7 @@ let localStorage = {
 
 let sessionStorage = {
   getItem: function(key) {
-    var value = key !== 'userInfo' ? window.sessionStorage.getItem(`test_${key}`) : window.sessionStorage.getItem(key)
+    var value = key !== 'userInfo' ? window.sessionStorage.getItem(`afSupportCenter_${key}`) : window.sessionStorage.getItem(key)
     if (value) {
       try {
         var value_json = JSON.parse(value)
@@ -58,14 +45,14 @@ let sessionStorage = {
   },
   setItem: function(key, value) {
     if (key !== 'userInfo') {
-      window.sessionStorage.setItem(`test_${key}`, value)
+      window.sessionStorage.setItem(`afSupportCenter_${key}`, value)
     } else {
       window.sessionStorage.setItem(key, value)
     }
   },
   removeItem: function(key) {
     if (key !== 'userInfo') {
-      window.sessionStorage.removeItem(`test_${key}`)
+      window.sessionStorage.removeItem(`afSupportCenter_${key}`)
     } else {
       window.localStorage.removeItem(key)
     }
